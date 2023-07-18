3d2d8895c71690d385e4465863af68db30dcf414
Matt30Curia <matteocu10@hotmail.com>
2023-07-18 23:13:26 +0200

    spawn added

---
+++ spawn.gd
@@ -0,0 +1,37 @@
+extends Node3D
+
+
+@export var enemyScene : PackedScene
+@export var maxSpawn: int
+@export var timer = 2.0  
+
+@export var Path: Path3D
+
+var spawnTimer = 0.0
+var spawnCount = 0
+
+func _createPath(enemy, pat):
+	
+	pat.add_child(PathFollow3D.new())
+	enemy.set_path(pat.get_child(0))
+	pat.get_child(0).add_child(enemy)
+	
+	return pat
+
+
+func _process(delta):
+	spawnTimer += delta
+	
+	if spawnTimer >= timer and maxSpawn > spawnCount :
+		var pat = Path.duplicate() 
+		spawnTimer = 0.0
+		spawnCount += 1
+		
+		var enemy = enemyScene.instantiate()
+		
+		enemy.transform.origin = Vector3(0,0,0)
+		
+		add_child(_createPath(enemy,pat))
+	else:
+		pass
+
