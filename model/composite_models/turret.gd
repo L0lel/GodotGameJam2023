db296103c4c060136bbf99ba6a7320daebfd5617
L0lel <lelemodr@gmail.com>
2023-07-20 11:49:08 +0200

    progresso nella torretta

--- model/composite_models/turret.gd
+++ model/composite_models/turret.gd
@@ -4,20 +4,24 @@ extends Node3D
 @onready var advancedTurret = preload("res://model/turret_double.obj")
 
 @onready var turret = get_node("Turret")
+#@onready var enemy_pos : Vector3 = get_node("res://turret_vision.gd").detected_enemy.position NON FUNZIONAAAA
 
 var enemy_seen : bool
-var init_yrotation = 0
+@export var rotation_velocity = 1
+
 
 func _ready():
+	self.rotation.y = 0
 	turret.mesh = baseTurret #setta la mesh della torretta a quella base
 	for child in $visionCast.get_children():
 		child.connect("visibile", spotted_enemy)
 
 func _process(delta):
 	if enemy_seen:
-		pass #TODO: guardare il nemico(look_at) e successivamente sparare
-		#implementado funzione shoot
+		#self.rotation.y = lerp_angle(self.rotation.y, look_at(enemy_pos,Vector3.UP), rotation_velocity*delta)
+		
+		#LOOK AT ritorna void
+		pass
 
 func spotted_enemy():
-	init_yrotation = rotation_degrees.y
 	enemy_seen = true
